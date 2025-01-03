6a9e338dd1a25d2fa3b2f58de94b1ac71a26dd9b
aryandi-putra <aryandi2712@gmail.com>
2025-01-03 16:37:24 +0700

    enable http request

--- app/src/main/java/com/aryandi/university/util/Util.kt
+++ app/src/main/java/com/aryandi/university/util/Util.kt
@@ -2,6 +2,6 @@ package com.aryandi.university.util
 
 class Util {
     companion object {
-        const val BASE_URL = "https://universities.hipolabs.com"
+        const val BASE_URL = "http://universities.hipolabs.com"
     }
 }
