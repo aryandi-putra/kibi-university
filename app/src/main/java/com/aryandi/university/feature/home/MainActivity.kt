ccd71aca4380f444a5b58f60cb0528f3dcc19efb
aryandi-putra <aryandi2712@gmail.com>
2025-01-08 15:13:43 +0700

    create compose preview

--- app/src/main/java/com/aryandi/university/feature/home/MainActivity.kt
+++ app/src/main/java/com/aryandi/university/feature/home/MainActivity.kt
@@ -24,13 +24,10 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -43,6 +40,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aryandi.university.domain.model.DataResult
 import com.aryandi.university.domain.model.University
@@ -54,91 +52,132 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContent {
-            var expanded by rememberSaveable { mutableStateOf(false) }
-            val searchText by viewModel.searchText.collectAsState()
+            MainContent()
+        }
+    }
 
-            UniversityTheme {
-                Scaffold(
-                    topBar = {
-                        SearchBar(
-                            modifier = Modifier.fillMaxWidth().semantics { traversalIndex = 0f },
-                            inputField = {
-                                SearchBarDefaults.InputField(
-                                    query = searchText,
-                                    onQueryChange = { viewModel.onSearchTextChange(it)  },
-                                    onSearch = { expanded = false },
-                                    expanded = expanded,
-                                    onExpandedChange = { expanded = it },
-                                    placeholder = { Text("Cari Universitas") },
-                                    leadingIcon = {
-                                        Icon(
-                                            Icons.Default.Search,
-                                            contentDescription = null
-                                        )
-                                    },
-                                    trailingIcon = {
-                                        Icon(
-                                            Icons.Default.MoreVert,
-                                            contentDescription = null
-                                        )
-                                    },
-                                )
-                            },
-                            expanded = expanded,
-                            onExpandedChange = { expanded = it },
-                            content = {}
-                        )
-                    },
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    Box(
+    @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
+    private fun MainContent() {
+        var expanded by rememberSaveable { mutableStateOf(false) }
+        val searchText by viewModel.searchText.collectAsState()
+        UniversityTheme {
+            Scaffold(
+                topBar = {
+                    SearchBar(
                         modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize()
-                            .padding(start = 4.dp, end = 4.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        val dataResult by viewModel.universities.collectAsState()
-                        when (dataResult) {
-                            is DataResult.Loading -> {
-                                CircularProgressIndicator(
-                                    modifier = Modifier.size(50.dp),
-                                    color = Color.Blue
-                                )
-                            }
-
-                            is DataResult.Error -> {
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    dataResult.error,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-
-                            is DataResult.Success -> {
-                                val universities = dataResult.data ?: emptyList()
-                                LazyColumn(
-                                    modifier = Modifier.fillMaxSize(),
-                                    verticalArrangement = Arrangement.spacedBy(10.dp)
-                                ) {
-                                    items(items = universities) {
-                                        UniversityItem(university = it)
-                                    }
-                                }
-                            }
-                        }
-                    }
+                            .fillMaxWidth()
+                            .semantics { traversalIndex = 0f },
+                        inputField = {
+                            SearchBarDefaults.InputField(
+                                query = searchText,
+                                onQueryChange = { viewModel.onSearchTextChange(it) },
+                                onSearch = { expanded = false },
+                                expanded = expanded,
+                                onExpandedChange = { expanded = it },
+                                placeholder = { Text("Cari Universitas") },
+                                leadingIcon = {
+                                    Icon(
+                                        Icons.Default.Search,
+                                        contentDescription = null
+                                    )
+                                },
+                                trailingIcon = {
+                                    Icon(
+                                        Icons.Default.MoreVert,
+                                        contentDescription = null
+                                    )
+                                },
+                            )
+                        },
+                        expanded = expanded,
+                        onExpandedChange = { expanded = it },
+                        content = {}
+                    )
+                },
+                modifier = Modifier.fillMaxSize()
+            ) { innerPadding ->
+                Box(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()
+                        .padding(start = 4.dp, end = 4.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    val dataResult by viewModel.universities.collectAsState()
+                    ListContent(dataResult)
                 }
             }
         }
     }
 }
+
+@Composable
+private fun ListContent(dataResult: DataResult<List<University>>) {
+    when (dataResult) {
+        is DataResult.Loading -> {
+            CircularProgressIndicator(
+                modifier = Modifier.size(50.dp),
+                color = Color.Blue
+            )
+        }
+
+        is DataResult.Error -> {
+            Toast.makeText(
+                LocalContext.current,
+                dataResult.error,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        is DataResult.Success -> {
+            val universities = dataResult.data ?: emptyList()
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                items(items = universities) {
+                    UniversityItem(university = it)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(
+    name = "University List",
+    showBackground = true
+)
+fun UniversityListPreview() {
+    ListContent(
+        DataResult.Success(
+            listOf(
+                University(
+                    "Institut Teknologi Bandung",
+                    "Indonesia",
+                    "www.itb.ac.id"
+                ),
+                University(
+                    "Universitas Indonesia",
+                    "Indonesia",
+                    "www.ui.ac.id"
+                ),
+                University(
+                    "Universitas Lampung",
+                    "Indonesia",
+                    "www.unila.ac.id"
+                ),
+            )
+        )
+    )
+}
+
 
 @Composable
 fun UniversityItem(university: University) {
@@ -158,4 +197,13 @@ fun UniversityItem(university: University) {
         Text(text = university.webPage)
         HorizontalDivider(modifier = Modifier.fillMaxWidth())
     }
+}
+
+@Composable
+@Preview(
+    name = "University Item",
+    showBackground = true
+)
+fun UniversityItemPreview() {
+    UniversityItem(University("Universitas Lampung", "Indonesia", "www.unila.ac.id"))
 }
